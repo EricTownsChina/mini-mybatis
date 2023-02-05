63086234ae9d3ad06df5078970c03b868b72c373
EricTownsChina <840017241@qq.com>
2023-02-05 15:05:40 +0800

    init

---
+++ src/main/java/priv/eric/mini/mybatis/MiniMybatisApplication.java
@@ -0,0 +1,13 @@
+package priv.eric.mini.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniMybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/priv/eric/mini/mybatis/MiniMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.eric.mini.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
