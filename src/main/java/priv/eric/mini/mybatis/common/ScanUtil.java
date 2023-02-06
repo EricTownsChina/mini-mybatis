218fd0c32536eb3a479dd2161c1abc356ee9b7b6
EricTownsChina <840017241@qq.com>
2023-02-06 13:51:50 +0800

    类型查找方法

---
+++ src/main/java/priv/eric/mini/mybatis/common/ScanUtil.java
@@ -0,0 +1,22 @@
+package priv.eric.mini.mybatis.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description: 类型查找发现工具类
+ *
+ * @author EricTowns
+ * @date 2023/2/6 13:49
+ */
+public class ScanUtil {
+
+    private ScanUtil() {
+    }
+
+    public static List<Class<?>> scanClasses(String packageName) {
+        // TODO: 2023/2/6 补全代码
+        return new ArrayList<>(0);
+    }
+
+}
